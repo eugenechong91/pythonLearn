824ad5144e0e6b2654b9905b36d0e238ed2a288c
Eugene Chong <eugene.chong@eventbase.com>
2017-11-23 00:58:34 -0800

    V1.0 Release

--- Models.py
+++ Models.py
@@ -1,116 +1,147 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import BigInteger
 
 Base = declarative_base()
+
+
 # Main Obj Declaration
 class Bot(Base):
     __tablename__ = 'bot'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
     name = Column(String, nullable=False)
     avatar = Column(String)
 
-    sources = relationship("BotSource", back_populates="bot")
-    intents = relationship("BotIntent", back_populates="bot")
-    intros = relationship("BotIntro", back_populates="bot")
-    suggestions = relationship("BotSuggestion", back_populates="bot")
+    sources = relationship("Source", secondary='bot_bot_source', back_populates="bots")
+    intents = relationship("Intent", secondary='bot_bot_intent_action', back_populates="bots")
+    #intros = relationship("Intro", secondary='bot_bot_introduction_message', back_populates="bots")
+
+    suggestions = relationship("BotSuggestion", backref="bots")
+    intros = relationship("BotIntro", backref="bots")
 
     def __repr__(self):
-        return"<Bot(id='%s', name='%s', avatar='%s')>" & (self.id, self.name, self.avatar)
-
-    def __init__(self, id, name, avatar):
-        self.id = id
+        return"<Bot(id='%s', name='%s', avatar='%s')>" % (self.id, self.name, self.avatar)
+    def __init__(self, name, avatar):
         self.name = name
         self.avatar = avatar
 
 class Source(Base):
     __tablename__ = 'bot_source'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
     name = Column(String, nullable=False)
 
-    bots = relationship("BotSource", back_populates="source")
+    bots = relationship("Bot", secondary='bot_bot_source', back_populates="sources")
 
     def __repr__(self):
-        return"<Bot(id='%s', name='%s')>" & (self.id, self.name)
+        return"<Bot(id='%s', name='%s')>" % (self.id, self.name)
+    def __init__(self, name):
+        self.name = name
 
 class Intent(Base):
     __tablename__ = 'bot_intent_action'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
     intent = Column(String, nullable=False)
     uri = Column(String, nullable=False)
 
-    bots = relationship("BotIntent", back_populates="intent")
+    bots = relationship("Bot", secondary='bot_bot_intent_action', back_populates="intents")
 
     def __repr__(self):
-        return"<Bot(id='%s', intent='%s', uri='%s')>" & (self.id, self.intent, self.uri)
+        return"<Bot(id='%s', intent='%s', uri='%s')>" % (self.id, self.intent, self.uri)
+    def __init__(self, intent, uri):
+        self.intent = intent
+        self.uri = uri
 
 class Intro(Base):
     __tablename__ = 'bot_introduction_message'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True, autoincrement=True)
     text = Column(String, nullable=False)
 
-    bots = relationship("BotIntro", back_populates="intro")
+    #bots = relationship("Bot", secondary='bot_bot_introduction_message', back_populates="intros")
 
     def __repr__(self):
-        return"<Bot(id='%s', text='%s')>" & (self.id, self.text)
+        return"<Bot(id='%s', text='%s')>" % (self.id, self.text)
+    def __init__(self, text):
+        self.text = text
 
 
 class Suggestion(Base):
     __tablename__ = 'bot_suggestion_message'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True, autoincrement=True)
     message = Column(String, nullable=False)
 
-    bots = relationship("BotSuggestion", back_populates="suggestion")
+    #bots = relationship("Bot", secondary='bot_bot_suggestion_message', back_populates="suggestions")
 
     def __repr__(self):
-        return"<Bot(id='%s', message='%s')>" & (self.id, self.message)
+        return"<Bot(id='%s', message='%s')>" % (self.id, self.message)
+    def __init__(self, message):
+        self.message = message
 
+
+class Setting(Base):
+    __tablename__ = 'settings_kv'
+
+    key = Column(String, primary_key=True, nullable=False)
+    value = Column(String)
+    data_type = Column(String)
+
+    def __repr__(self):
+        return "<Setting(key='%s', value='%s', data_type='%s')>" % (self.key, self.value, self.data_type)
 
 # Everything is a many to many relationship....
 class BotSource(Base):
     __tablename__ = 'bot_bot_source'
 
-    id = Column(Integer, primary_key=True)
-    bot_id = Column(Integer, ForeignKey('bot.id'), primary_key=True)
-    bot_source_id = Column(Integer, ForeignKey('bot_source.id'), primary_key=True)
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True, autoincrement=True)
+    bot_id = Column(Integer, ForeignKey('bot.id'))
+    bot_source_id = Column(Integer, ForeignKey('bot_source.id'))
 
-    bot = relationship("Bot", back_populates="sources")
-    source = relationship("Source", back_populates="bots")
+    # bot = relationship("Bot", back_populates="sources")
+    # source = relationship("Source", back_populates="bots")
 
 class BotIntent(Base):
     __tablename__ = 'bot_bot_intent_action'
 
-    id = Column(Integer, primary_key=True)
-    bot_id = Column(Integer, ForeignKey('bot.id'), primary_key=True)
-    bot_intent_id = Column(Integer, ForeignKey('bot_intent_action.id'), primary_key=True)
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True, autoincrement=True)
+    bot_id = Column(Integer, ForeignKey('bot.id'))
+    bot_intent_id = Column(Integer, ForeignKey('bot_intent_action.id'))
 
-    bot = relationship("Bot", back_populates="intents")
-    intent = relationship("Intent", back_populates="bots")
+    # bot = relationship("Bot", back_populates="intents")
+    # intent = relationship("Intent", back_populates="bots")
 
+# Associate OBJ since extra field
 class BotIntro(Base):
     __tablename__ = 'bot_bot_introduction_message'
 
-    id = Column(Integer, primary_key=True)
-    bot_id = Column(Integer, ForeignKey('bot.id'), primary_key=True)
-    bot_introduction_message_id = Column(Integer, ForeignKey('bot_introduction_message.id'), primary_key=True)
-    sort_order = Column(Integer, nullable=False)
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True, autoincrement=True)
+    bot_id = Column(Integer, ForeignKey('bot.id'))
+    bot_introduction_message_id = Column(Integer, ForeignKey('bot_introduction_message.id'))
+    sort_order = Column(Integer, nullable=False, autoincrement=True)
 
-    bot = relationship("Bot", back_populates="intros")
-    intro = relationship("Intro", back_populates="bots")
+    # bot = relationship("Bot", backref="intros")
+    intro = relationship("Intro", backref="bot")
 
+# botIntroLinker = Table('bot_bot_introduction_message', Base.metadata,
+#                        Column('bot_id', Integer, ForeignKey('bot.id'), primary_key=True),
+#                        Column('bot_introduction_message_id', Integer, ForeignKey('bot_introduction_message.id'), primary_key=True),
+#                        Column('id', BigInteger().with_variant(Integer, "sqlite"), nullable=True),
+#
+#                        Column('sort_order', Integer, nullable=True ))
+
+# Associate OBJ since extra field
 class BotSuggestion(Base):
     __tablename__ = 'bot_bot_suggestion_message'
 
-    id = Column(Integer, primary_key=True)
-    bot_id = Column(Integer, ForeignKey('bot.id'), primary_key=True)
-    bot_intent_id = Column(Integer, ForeignKey('bot_suggestion_message.id'), primary_key=True)
-    sort_order = Column(Integer, nullable=False)
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True, autoincrement=True)
+    bot_id = Column(Integer, ForeignKey('bot.id'))
+    bot_intent_id = Column(Integer, ForeignKey('bot_suggestion_message.id'))
+    sort_order = Column(Integer, nullable=False, autoincrement=True)
 
-    bot = relationship("Bot", back_populates="suggestions")
-    suggestion = relationship("Suggestion", back_populates="bots")
+    # bot = relationship("Bot", back_populates="suggestions")
+    suggestion = relationship("Suggestion", backref="bot")
 
--- mainScipt.py
+++ mainScipt.py
@@ -1,42 +1,196 @@
+
 import sqlite3 as MySql
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-
-
-Session = sessionmaker()
-
-# connection = MySql.connect('AndyDB.sqlite')
-# c = connection.cursor()
+from sqlalchemy.orm import exc
+import sys
 
 # Create Engine
-engine = create_engine('sqlite:///AndyDB.sqlite', echo=True)
+#engine = create_engine('sqlite:///AndyDB.sqlite', echo=False)
+engine = create_engine('sqlite:///'+sys.argv[1], echo=False)
 
-# Create handles to DB via Sessions
+# Create Session to handle DB
+Session = sessionmaker()
 Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Session.configure(bind=engine)
 session = Session()
 
 # Initialize Table Structure
-from Models import Bot, Suggestion, Intro, Source, Intent, Base
-# metadata = Base.metadata.declarative_base(bind=engine)
+from Models import Bot, Suggestion, Intro, Source, Intent, Setting, Base, BotIntro, BotSuggestion
 Base.metadata.create_all(engine)
 
-print(Base.metadata.tables.keys())
-print(Base.metadata.reflect(engine))
-print('1231231231231231')
+# These are setting defaults [(key,date_type, value),(key,date_type,value)]
+settingDefaults = [ ('SEMANTICS_conversation_bot_invalid_compound_message',         'text',     "Oops! Looks like I didn't find anything"),
+                    ('CONFIG_conversation_bot_intent_fallback_suggestion_count',    'text',     '3'),
+                    ('CONFIG_conversation_bot_intent_fallback',                     'text',     'Default Fallback Intent'),
+                    ('CONFIG_conversation_bot_intent_suggestion',                   'text',     'Suggestion'),
+                    ('CONFIG_conversation_bot_compound_intent_map',                 'text',     '{  "FEED_session": "session", \
+                                                                                                    "FEED_speaker": "speaker", \
+                                                                                                    "FEED_artist": "artist", \
+                                                                                                    "FEED_exhibitors": "exhibitors", \
+                                                                                                    "FEED_location": "location", \
+                                                                                                    "FEED_entity": "entity" }'),
+                    ('CONFIG_conversation_bot_data_key_map_eventbase',              'text',     '{  "session": "FEED_session", \
+                                                                                                    "speaker": "FEED_speaker", \
+                                                                                                    "artist": "FEED_artist", \
+                                                                                                    "exhibitors": "FEED_exhibitors", \
+                                                                                                    "location": "FEED_location", \
+                                                                                                    "entity": "FEED_entity" }')]
+# These are the bot table defaults
+tBotDefault = [('Bot Name', 'bot_avatar_ebhi')]
+tSourceDefault = [('eventbase')]
+tIntentDefault = [('DL_Favorites', '/favorites'),
+                  ('DL_ScheduleView', '/schedule'),
+                  ('DL_ProfileView', '/my_profile')]
+tIntroDefault = [("Hello I'm a personal assistant."),
+                 ("Here are some sample questions you can ask me:")]
+tSuggestionDefault = [("What's on my schedule?"),
+                      ("Are there any roundtable sessions?"),
+                      ("Where are the breakout sessions?")]
+
+SORT_FACTOR = 10
+
+# # STEP 3: Go through the Settings_KV Table First
+# If it exist and has a value, ask if user wants to overwrite
+# If it exist but is NULL or "", update
+# If it does not exist, add.
+# TODO: Handle If more than 1 row returned?
+for setting in settingDefaults:
+    try:
+        # Search for one instance
+        row = session.query(Setting).filter(Setting.key == setting[0], Setting.value != None).one()
+
+        # If the value is empty, it should be considered as NoResults
+        if not row.value:
+            raise exc.NoResultFound()
+        # else ask user if they want to overwrite
+        else:
+            print(setting[0] + ' was found with the following value: \n"' + row.value + '"')
+            choice = input("Do you want to overwrite with default? [Yes/No]")
+            if choice.lower() in {'yes', 'ye', 'y'}:
+            # Overwrite it
+                record = session.query(Setting).filter(Setting.key == setting[0]).one()
+                print('Adding: ' + setting[0] + '\n Reason: Manual overwrite')
+                record.value = setting[2]
+                record.data_type = setting[1]
+    # If more than 1... raise TODO not sure what to do here yet
+    except exc.MultipleResultsFound:
+        print("ERROR: More than 1 " + setting[0] + " was found!")
+    except exc.NoResultFound:
+        try:
+            # Filter for just the key to encompass the "NULL" values
+            record = session.query(Setting).filter(Setting.key == setting[0]).one()
+
+            # If the key exists, but no value assigned, UPDATE
+            print('Adding: ' + setting[0] + '\n Reason: No value found')
+            record.value = setting[2]
+            record.data_type = setting[1]
+        # otherwise exception thrown if not found, ie. will just ADD
+        except exc.NoResultFound:
+            print('Adding: ' + setting[0] + '\n Reason: No key found')
+            keyToAdd = Setting(key=setting[0], value=setting[2], data_type=setting[1])
+            session.add(keyToAdd)
+    # Push and Clear
+    session.commit()
+    session.flush()
 
 
-# Creating First Instance of Bot ie...a row
-myBot = Bot(name='TestName', avatar= 'TestAvi', id=2)
-session.add(myBot)
-session.commit()
 
+# Step 5: Populate "bot" Tables
+# If Nothing, just add all the defaults above
+if (session.query(Bot).count() == 0):
+    for eachBot in tBotDefault:
+        botToAdd = Bot(name=eachBot[0], avatar=eachBot[1])
+        botToAdd.sources.append(Source(name=tSourceDefault[tBotDefault.index(eachBot)]))
 
+        for eachIntent in tIntentDefault:
+            botToAdd.intents.append(Intent(intent=eachIntent[0],uri=eachIntent[1]))
 
-# c.execute('''CREATE TABLE myTable
-#                 (col1 text, col2 text, col3 text)''')
-# c.execute('''INSERT INTO myTable VALUES ('col1val', 'col2val', 'col3val')''')
-#
-# connection.commit()
-#
-# connection.close()
+        for eachIntro in tIntroDefault:
+            inter = BotIntro(sort_order=((1+tIntroDefault.index(eachIntro))*SORT_FACTOR))
+            inter.intro = Intro(text=eachIntro)
+            botToAdd.intros.append(inter)
+
+        for eachSuggestion in tSuggestionDefault:
+            inter = BotSuggestion(sort_order=((1+tSuggestionDefault.index(eachSuggestion))*SORT_FACTOR))
+            inter.suggestion = Suggestion(message=eachSuggestion)
+            botToAdd.suggestions.append(inter)
+
+        session.add(botToAdd)
+        session.commit()
+        session.flush()
+else:
+    choice = input("You already have a bot added. Would you like to add another? [Yes/No]")
+    if choice.lower() in {'yes', 'ye', 'y'}:
+        nameInput = input("Enter the name of the bot:") or tBotDefault[0][0]
+        avatarInput = input("Enter the avatar of the bot:") or tBotDefault[0][1]
+        botToAdd = Bot(name=nameInput, avatar=avatarInput)
+
+        sourceInput = input("What is the source of the bot?") or tSourceDefault[0]
+        botToAdd.sources.append(Source(name=sourceInput))
+
+        # Intent Input, if no, askes if default is desired
+        intentChoice = input("Do you want to add your own Intents? [Yes/No]")
+        if intentChoice.lower() in {'yes', 'ye', 'y'}:
+            continueIntent = True
+            while continueIntent:
+                intentInput = input("Enter your intent: ex. DL_Favorites") or tIntentDefault[0][0]
+                uriInput = input("Enter the uri for this " + intentInput + " intent: ex. /favorites") or tIntentDefault[0][1]
+                botToAdd.intents.append(Intent(intent=intentInput, uri=uriInput))
+                continueChoice = input("Do you want to add another intent?")
+                if continueChoice.lower() in {'no', 'n', ''}:
+                    continueIntent = False
+        else:
+            intentChoice = input("Do you want to use default Intents? [Yes/No]")
+            if intentChoice.lower() in {'yes', 'ye', 'y'}:
+                for eachIntent in tIntentDefault:
+                    botToAdd.intents.append(Intent(intent=eachIntent[0], uri=eachIntent[1]))
+
+        # Intro Input, if no, askes if default is desired
+        introChoice = input("Do you want to add your own Introduction Messages? [Yes/No]")
+        if introChoice.lower() in {'yes', 'ye', 'y'}:
+            continueIntro = True
+            index = 0
+            while continueIntro:
+                index += 1
+                introInput = input("In order, enter your Introduction Message: ex. 'Hello I'm a personal assistant.'") or tIntroDefault[0]
+                inter = BotIntro(sort_order=(index*SORT_FACTOR))
+                inter.intro = Intro(text=introInput)
+                botToAdd.intros.append(inter)
+                continueChoice = input("Do you want to add another Introduction Message?")
+                if continueChoice.lower() in {'no', 'n', ''}:
+                    continueIntro = False
+        else:
+            introChoice = input("Do you want to use default Introduction Messages? [Yes/No]")
+            if introChoice.lower() in {'yes', 'ye', 'y'}:
+                for eachIntro in tIntroDefault:
+                    inter = BotIntro(sort_order=((1 + tIntroDefault.index(eachIntro)) * SORT_FACTOR))
+                    inter.intro = Intro(text=eachIntro)
+                    botToAdd.intros.append(inter)
+
+        # Suggestion Input, if no, askes if default is desired
+        suggestChoice = input("Do you want to add your own Suggestion Messages? [Yes/No]")
+        if suggestChoice.lower() in {'yes', 'ye', 'y'}:
+            continueSuggest = True
+            index = 0
+            while continueSuggest:
+                index += 1
+                introInput = input("In order, enter your Suggestion Message: ex. 'What's on my schedule?'") or tSuggestionDefault[0]
+                inter = BotSuggestion(sort_order=(index * SORT_FACTOR))
+                inter.suggestion = Suggestion(message=introInput)
+                botToAdd.suggestions.append(inter)
+                continueChoice = input("Do you want to add another Suggestion Message?")
+                if continueChoice.lower() in {'no', 'n', ''}:
+                    continueSuggest = False
+        else:
+            suggestChoice = input("Do you want to use default Suggestion Messages? [Yes/No]")
+            if suggestChoice.lower() in {'yes', 'ye', 'y'}:
+                for eachSuggestion in tSuggestionDefault:
+                    inter = BotSuggestion(sort_order=((1 + tSuggestionDefault.index(eachSuggestion)) * SORT_FACTOR))
+                    inter.suggestion = Suggestion(message=eachSuggestion)
+                    botToAdd.suggestions.append(inter)
+
+        # After Input Phase is done, add
+        session.add(botToAdd)
+        session.commit()
+        session.flush()
